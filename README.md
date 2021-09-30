6bdeeec5303e5cc57b55aca0b191d9dfb90ce4c3
Bojan Gajic <bojan.gajic@crealogix.com>
2021-09-30 11:17:23 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # vending-machine-api
+Design an API for a vending machine, allowing users with a “seller” role to add, update or remove products, while users with a “buyer” role can deposit coins into the machine and make purchases. Your vending machine
+should only accept 5, 10, 20, 50 and 100 cent coins
+
+Tasks
+
+REST API should be implemented consuming and producing “application/json”
+Implement product model with amountAvailable, cost, productName and sellerId fields
+Implement user model with username, password, deposit and role fields
+Implement CRUD for users (POST shouldn’t require authentication)
+Implement CRUD for a product model (GET can be called by anyone, while POST, PUT and DELETE can be called only by the seller user who created the product)
+Implement /deposit endpoint so users with a “buyer” role can deposit 5, 10, 20, 50 and 100 cent coins into their vending machine account
+Implement /buy endpoint (accepts productId, amount of products) so users with a “buyer” role can buy products with the money they’ve deposited. API should return total they’ve spent, products they’ve purchased and their
+change if there’s any (in 5, 10, 20, 50 and 100 cent coins)
+
+Implement /reset endpoint so users with a “buyer” role can reset their deposit
--- src/main/java/com/bojan/vending/service/TransactionServiceImpl.java
+++ src/main/java/com/bojan/vending/service/TransactionServiceImpl.java
@@ -1,8 +1,12 @@
 package com.bojan.vending.service;
 
+import com.bojan.vending.dto.BuyRequest;
+import com.bojan.vending.dto.BuyResponse;
 import com.bojan.vending.dto.CoinDto;
 import com.bojan.vending.dto.mapper.CoinMapper;
+import com.bojan.vending.exception.AmountNotSufficientException;
 import com.bojan.vending.exception.EntityNotFoundException;
+import com.bojan.vending.model.Product;
 import com.bojan.vending.model.User;
 import com.bojan.vending.repository.CoinRepository;
 import com.bojan.vending.repository.ProductRepository;
@@ -20,12 +24,69 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public int deposit(CoinDto coinDto, String username) {
-        int total = 0;
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new EntityNotFoundException("User not found with username:" + username));
-        total = user.getDeposit() + CoinMapper.calculateTotal(coinDto);
+        int total = user.getDeposit() + CoinMapper.calculateTotal(coinDto);
         user.setDeposit(total);
         userRepository.save(user);
         return total;
+    }
+
+    @Override
+    public synchronized BuyResponse buy(BuyRequest request, String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new EntityNotFoundException("User not found with username:" + username));
+        Product product = productRepository.findById(request.getProductId())
+                .orElseThrow(() -> new EntityNotFoundException("Product not found with ID:" + request.getProductId()));
+        if (product.getAmountAvailable() < request.getAmount()) {
+            throw new AmountNotSufficientException("Not sufficient amount of product with ID: " + request.getProductId());
+        }
+
+        int totalCost = request.getAmount() * product.getCost();
+        if (user.getDeposit() < totalCost) {
+            throw new AmountNotSufficientException("Not enough coins deposited!");
+        }
+        CoinDto change = calculateChange(user.getDeposit(), totalCost);
+
+        BuyResponse response = BuyResponse.builder()
+                .productName(product.getProductName())
+                .deposited(user.getDeposit())
+                .totalSpent(totalCost)
+                .change(change)
+                .build();
+
+        product.setAmountAvailable(product.getAmountAvailable() - request.getAmount());
+        productRepository.save(product);
+
+        user.setDeposit(0);
+        userRepository.save(user);
+
+        return response;
+    }
+
+    private CoinDto calculateChange(int deposit, int price) {
+        CoinDto result = new CoinDto();
+        int change = deposit - price;
+
+        int dollars = change / 100;
+        result.setDollar(dollars);
+        change = change % 100;
+
+        int half = change / 50;
+        result.setHalf(half);
+        change = change % 50;
+
+        int fifth = change / 20;
+        result.setFifth(fifth);
+        change = change % 20;
+
+        int dime = change / 10;
+        result.setDime(dime);
+        change = change % 10;
+
+        int nickel = change / 5;
+        result.setNickel(nickel);
+
+        return result;
     }
 }
